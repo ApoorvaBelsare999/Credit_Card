cb13d113b81a4491ca31fc559f060d8fcba516c2
ApoorvaBelsare999 <56408893+ApoorvaBelsare999@users.noreply.github.com>
2022-07-06 15:36:52 +0530

    Update Visualization.py

--- app/pages/Visualization.py
+++ app/pages/Visualization.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+import streamlit.components.v1 as components
 from urllib.error import URLError
 
 st.set_page_config(page_title="Visualization", page_icon="📈")
