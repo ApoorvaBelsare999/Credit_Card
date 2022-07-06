419f76b3f5c20e26e39ae2726effbe004713ccd0
ApoorvaBelsare999 <56408893+ApoorvaBelsare999@users.noreply.github.com>
2022-07-06 14:41:48 +0530

    Update dataset.py

--- app/pages/dataset.py
+++ app/pages/dataset.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+from urllib.error import URLError
 
+st.set_page_config(page_title="DataFrame Demo", page_icon="📊")
+
+
+st.markdown("# DataFrame Demo")
+data = pd.read_csv('default of credit card clients.csv')
+st.write(data)
